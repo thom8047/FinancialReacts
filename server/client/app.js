3dfafac1ac2c926c83abec18e9b5a25d30011ef1
thom8047 <thom8047@bears.unco.edu>
2021-09-26 13:58:50 -0600

    Updated beta branch to contain current updates; pull into main for finished product and reject what doesn't make the cut

--- server/client/app.js
+++ server/client/app.js
@@ -12,6 +12,9 @@ class Design extends React.Component {
             user: props.user,
         };
     }
+    changeCurrentVisuals(id) {
+        console.log(id);
+    }
     // RENDER METHOD
     render() {
         // can only return a single div!
@@ -21,16 +24,18 @@ class Design extends React.Component {
                 <div className="welcome"> <h1 id="title">{this.state.title}, Welcome {this.state.user}  </h1> </div>
                 {/* Navbar for toggling through charts/visuals */}
                 <div className="visuals">
-                    <span id="test">ACCOUNT SUMMARY  |  </span>
-                    <span id="test">CHARGES  |  </span>
-                    <span id="test">SAVINGS  |  </span>
+                    <span>
+                        <button onClick={(event) => { this.changeCurrentVisuals("sum"); }} className="sum" id="header">ACCOUNT SUMMARY</button>
+                        <button onClick={(event) => { this.changeCurrentVisuals("charge"); }} className="charge" id="header">CHARGES</button>
+                        <button onClick={(event) => { this.changeCurrentVisuals("save"); }} className="save" id="header">SAVINGS</button>
+                    </span>
                 </div>
 
                 {/* Inputs for entering data to be included in visuals */}
                 <div className="user-inputs">
-                    <span id="test">INCOME  |  </span>
-                    <span id="test">EXPENSES  |  </span>
-                    <span id="test">GOALS  |  </span>
+                    <span id="header">INCOME</span>
+                    <span id="header">EXPENSES</span>
+                    <span id="header">GOALS</span>
                 </div>
             </div>
         );
